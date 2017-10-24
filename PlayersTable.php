0dbd5d8ffce52d6d6667c7ac4b423035daff562e
fdenoel <florentdenoel@gmail.com>
2017-10-24 14:41:56 +0200

    Add files via upload

---
+++ PlayersTable.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Model/Table/UsersTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class UsersTable extends Table
+{
+
+    public function validationDefault(Validator $validator)
+    {
+        return $validator
+            ->notEmpty('email', 'An email is required')
+            ->notEmpty('password', 'A password is required')
+            ->notEmpty('role', 'A role is required');
+    }
+
+}
+
+?>
