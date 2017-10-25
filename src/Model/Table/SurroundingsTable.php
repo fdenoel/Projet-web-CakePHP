bbe814e3f6a31b4dc8c5127112ef78d9fb4ad612
guibioy <32839481+guibioy@users.noreply.github.com>
2017-10-25 12:53:54 +0200

    Add files via upload

--- src/Model/Table/SurroundingsTable.php
+++ src/Model/Table/SurroundingsTable.php
@@ -2,12 +2,29 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
 
 class SurroundingsTable extends Table
 {
 	
 	public function arene(){
 		return $this->find('all');
+	}
+
+	public function insertion($id, $type, $x, $y){
+		$suroundings = TableRegistry::get('surroundings');
+		$query = $suroundings->query();
+		$query->insert(['id', 'type', 'coordinate_x', 'coordinate_y'])->values(['id' => $id, 'type' => $type, 'coordinate_x' =>$x, 'coordinate_y' => $y])->execute();
+	}
+
+	public function suppression()
+	{
+		return $this->deleteAll(['id' < 40 ]);
+	}
+
+	public function suppressionMonstre()
+	{
+		return $this->deleteAll(['type' == 'W' ]);
 	}
 
 }
