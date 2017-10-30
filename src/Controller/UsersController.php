91fccb373895e3f72af66b24e7273baebd450b2c
guibioy <32839481+guibioy@users.noreply.github.com>
2017-10-30 16:38:10 +0100

    Add files via upload

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,30 +1,25 @@
 <?php
-
 // src/Controller/UsersController.php
 namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Event\Event;
 class UsersController extends AppController
 {
-
     public function beforeFilter(Event $event)
         {
         parent::beforeFilter($event);
         $this->loadModel('Players');
         $this->Auth->allow(['add', 'logout', 'login']);
         }
-
     public function index()
         {
         $this->set('user', $this->Players->find('all'));
         }
-
     public function view($id)
         {
         $user = $this->Players->get($id);
         $this->set(compact('user'));
         }
-
     public function add()
     {
     $user = $this->Players->newEntity();
@@ -39,7 +34,6 @@ class UsersController extends AppController
             }
         $this->set('user', $user);
         }
-
     public function login()
     {
         if ($this->request->is('post')) {
@@ -47,18 +41,13 @@ class UsersController extends AppController
             if ($user) {
                 $this->Auth->setUser($user);
                 return $this->redirect($this->Auth->redirectUrl());
-
             }
             $this->Flash->error(__('Invalid username or password, try again'));
         }
     }
-
-
     public function logout()
     {
         return $this->redirect($this->Auth->logout());
     }
-
 }
-
 ?>
